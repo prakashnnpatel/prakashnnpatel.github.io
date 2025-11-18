c6a212613ec54de08fc4f945990b57b8a1240703
unknown <prakashnnpatel@gmail.com>
2025-11-18 17:04:17 +0530

    Portfolio added

--- js/portfolio.js
+++ js/portfolio.js
@@ -26,6 +26,17 @@
     $('.back-to-top').click(function () {
         $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
         return false;
-    });    
+    });
+
+    document.addEventListener('contextmenu', event => event.preventDefault());
+    document.onkeydown = function(e) {
+        if (e.keyCode == 123) return false; // F12
+        if (e.ctrlKey && e.shiftKey && e.keyCode == 73) return false; // Ctrl+Shift+I
+        if (e.ctrlKey && e.shiftKey && e.keyCode == 74) return false; // Ctrl+Shift+J
+        if (e.ctrlKey && e.keyCode == 85) return false; // Ctrl+U
+        if (e.ctrlKey && e.keyCode == 83) return false; // Ctrl+S
+    };
+
+
 })(jQuery);
 
