aeec8fe05f62a1e194ab3a7df8f538334f2122fc
unknown <prakashnnpatel@gmail.com>
2025-12-08 20:04:55 +0530

    Timeout added on image loading

--- js/main.js
+++ js/main.js
@@ -109,17 +109,18 @@
 
 
     // Portfolio isotope and filter
-    var portfolioIsotope = $('.portfolio-container').isotope({
-        itemSelector: '.portfolio-item',
-        layoutMode: 'fitRows'
-    });
-    $('#portfolio-flters li').on('click', function () {
-        $("#portfolio-flters li").removeClass('active');
-        $(this).addClass('active');
+    setTimeout(function() {
+        var portfolioIsotope = $('.portfolio-container').isotope({
+            itemSelector: '.portfolio-item',
+            layoutMode: 'fitRows'
+        });
+        $('#portfolio-flters li').on('click', function () {
+            $("#portfolio-flters li").removeClass('active');
+            $(this).addClass('active');
 
-        portfolioIsotope.isotope({filter: $(this).data('filter')});
-    });
-
+            portfolioIsotope.isotope({filter: $(this).data('filter')});
+        });
+    }, 1000);
 
     // Testimonials carousel
     $(".testimonial-carousel").owlCarousel({
